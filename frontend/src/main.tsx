f304ccc890e49f8b49efdec87904ed8bfdbd4f3d
Alfaducei <djmillhouse@outlook.com>
2026-04-19 12:53:30 -0400

    feat(ui): swap to green-earth palette + next-themes dark/light toggle

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Toaster } from '@/components/ui/sonner'
+import { ThemeProvider } from '@/components/theme-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster richColors position="bottom-right" closeButton />
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+      storageKey="tradewise-theme"
+    >
+      <App />
+      <Toaster richColors position="bottom-right" closeButton />
+    </ThemeProvider>
   </StrictMode>
 )
